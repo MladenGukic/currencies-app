d576a6c61b4149ef5e148200927c322d932c727f
MladenGukic <53443353+MladenGukic@users.noreply.github.com>
2022-09-14 12:58:06 +0200

    fix: fixed header style

--- src/components/SplitHeader.tsx
+++ src/components/SplitHeader.tsx
@@ -1,48 +1,41 @@
 import styled from "styled-components";
-import logo from "../logocurr.png";
+import logo from "../assets/logocurr.png";
 
 export const SplitHeader = () => {
   return (
     <StyledSplitHeader>
-      <body>
-        <div className="topnav">
-          <img src={logo} alt="Logo" className="logo" />
-          <a className="active" href="/currencies">
-            € Currencies
-          </a>
-        </div>
-      </body>
+      <LogoCurrency alt="logo" src={logo} />
+      <Active className="active" href="/currencies">
+        € Currencies
+      </Active>
     </StyledSplitHeader>
   );
 };
 
-const StyledSplitHeader = styled.body`
+const StyledSplitHeader = styled.div`
   margin: 0;
   font-family: Arial, Helvetica, sans-serif;
+  overflow: hidden;
+  background: linear-gradient(
+    to right,
+    #e35b00 0px,
+    #e35b00 480px,
+    #ff6600 480px,
+    #ff6600 1265px
+  );
+`;
 
-  .logo {
-    margin-left: -600px;
-  }
+const LogoCurrency = styled.img`
+  margin-left: -600px;
+`;
 
-  .topnav {
-    overflow: hidden;
-
-    background: linear-gradient(
-      to right,
-      #e35b00 0px,
-      #e35b00 480px,
-      #ff6600 480px,
-      #ff6600 1265px
-    );
-  }
-
-  .active {
-    margin-left: 500px;
-    float: left;
-    color: #f2f2f2;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-size: 17px;
-  }
+const Active = styled.a`
+margin-left: 500px;
+float: left;
+color: #f2f2f2;
+text-align: center;
+padding: 14px 16px;
+text-decoration: none;
+font-size: 17px;
+}
 `;
