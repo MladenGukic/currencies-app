16248707afd025ba654476123fdbce054aeb0e07
MladenGukic <53443353+MladenGukic@users.noreply.github.com>
2022-09-16 18:10:02 +0200

    feat: created a route that leads to the add-currency page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import { SplitHeader } from "./components/SplitHeader";
-import {AddEditCurrency} from "./components/AddEditCurrency"
+import { AddEditCurrency } from "./components/AddEditCurrency";
 function App() {
   return (
     <>
       <SplitHeader />
       <Routes>
         <Route path="/" element={<Navigate to="/currencies" />} />
-        <Route path="add" element={<AddEditCurrency/>} />
+        <Route path="currencies/add" element={<AddEditCurrency />} />
       </Routes>
     </>
   );
